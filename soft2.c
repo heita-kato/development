fbf59ba75273ebd38d5378c1094084fa3651558c
heita-kato <129523656+heita-kato@users.noreply.github.com>
2024-06-23 11:37:57 +0900

    Add files via upload

---
+++ soft2.c
@@ -0,0 +1,95 @@
+/*
+ ============================================================================
+ Name        : soft1.c
+ Author      : 加藤平大23122011
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//本課題においては最大値抽出法のアルゴリズムを使用した。
+
+void inputfunc(FILE *fp, int *a, int *c);
+void sortfunc(int *a, int *c);
+void outputfunc(FILE *fp, int *a, int *c);
+
+int main(int argc, char *argv[])
+{
+    //エラー処理
+    FILE *fpi, *fpo;
+    int array[10];
+    int cnt = 0;
+    if (argc != 3) {
+        fprintf(stderr, "Illegal number of argument.\n");
+        return (-1);
+    }
+
+    if ((fpi = fopen(argv[1], "r")) == NULL) {
+        fprintf(stderr, "Can't open input file <%s>.\n", argv[1]);
+        return (-1);
+    }
+
+    if ((fpo = fopen(argv[2], "w")) == NULL) {
+        fprintf(stderr, "Can't open output file <%s>.\n", argv[2]);
+        return (-1);
+    }
+//入力
+inputfunc(fpi, array, &cnt);
+//ソート
+sortfunc(array, &cnt);
+//出力
+outputfunc(fpo, array, &cnt);
+    fclose(fpi);
+    fclose(fpo);
+    return (0);
+}
+
+//入力関数の定義
+void inputfunc(FILE *fp, int *a, int *c)
+{
+    int meta = 0;
+    int ch;
+     while( ( ch = fscanf( fp, "%d", &meta) ) != EOF ){
+        if ((*c) > 9){
+            printf("Too many integer numbers in infile.\nMax is 10.");
+            exit(0);
+        }
+        a[(*c)] = meta;
+        //printf( "%d\n", a[(*c)]);
+        (*c)++;
+  }
+  
+}
+
+//ソート関数の定義
+void sortfunc(int *a, int *c)
+{
+    int i,j;
+    int temp;
+    int max;
+    for (j=0;j<(*c);j++)
+    {
+        max = 0;
+    for (i=0;i<(*c)-j;i++){
+        if (a[i+1] > a[max]){
+            max = i+1;
+        }
+    }
+    temp = a[max];
+    a[max] = a[(*c)-j];
+    a[(*c)-j] = temp;
+    }
+}
+
+//出力関数の定義
+void outputfunc(FILE *fp, int *a, int *c)
+{
+    int i;
+    for (i=0;i<(*c);i++){
+        fprintf(fp, "%d ", a[i]);
+    }
+}
